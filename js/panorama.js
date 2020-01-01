5754c90e9626525f7eb19a44a9492c09dcb65a95
bingzhuwuhen <1209157727@qq.com>
2020-01-01 22:32:47 +0800

    Site updated: 2020-01-01 22:32:46

---
+++ js/panorama.js
@@ -0,0 +1,58 @@
+var fullwrap = document.getElementById('fullwrap');
+    var picArr = [];
+    if ($render_data && $render_data.data && $render_data.data.img_url) {
+        var img = new Image();
+        // img.src = $render_data.data.img_url;
+        var img_url = $render_data.data.img_url;
+        img.className = 'hid';
+        img.crossOrigin = '';
+        img.src = img_url;
+        fullwrap.appendChild(img);
+        if (img.complete) {
+            imgLoaded(img_url);
+            fullwrap.removeChild(img);
+        }
+        else {
+            img.addEventListener('load', function () {
+                imgLoaded(img_url);
+                fullwrap.removeChild(img);
+            });
+        }
+        img.addEventListener('error', function() {
+            // 展示错误信息
+            var errors = document.getElementById('errors');
+            if (errors) {
+                errors.className = 'errors';
+
+            }
+            if(fullwrap){
+                fullwrap.className = 'wrap';
+                fullwrap.removeChild(img);
+            }
+        });
+    }
+
+    function imgLoaded(url) {
+        picArr.push(url);
+        if (fullwrap) {
+            var t = new Panorama({
+                el: fullwrap,
+                picArray: picArr,
+                canvasW: fullwrap.offsetWidth,
+                canvasH: fullwrap.offsetHeight,
+                autoRander: true,
+                closeDeviceOri: false,
+            });
+            // pc如果需要兼容屏幕改变事件示例:
+            window.addEventListener('resize', function () {
+                if (t) {
+                    t.onResize(fullwrap.offsetWidth, fullwrap.offsetHeight);
+                }
+            }, false);
+            fullwrap.classList = 'wrap';
+        }
+    }
+
+    /* document.getElementById('back').onclick = function () {
+        window.history.back();
+    }*/
